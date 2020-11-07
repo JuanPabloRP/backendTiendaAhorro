12880179853a51664569178031100a5a3054cc73
Juan Pablo Roldán Patiño <56945649+JuanPabloRP@users.noreply.github.com>
2020-11-07 11:51:03 -0500

    registro funcional

--- routes/Productos.js
+++ routes/Productos.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
 const mysqlConnection = require("../db/db");
+
 
 router.get("/home", (req, res) => {
   mysqlConnection.query("SELECT * FROM Productos ", (err, rows, fields) => {
@@ -14,7 +14,8 @@ router.get("/home", (req, res) => {
 });
 
 router.get("/buscarProductos", (req, res) => {
-  mysqlConnection.query("SELECT * FROM Productos ", (err, rows, fields) => {
+ let {buscaProd}=req.query
+  mysqlConnection.query(`SELECT * FROM Productos WHERE Productos LIKE '${buscaProd}%'`, (err, rows, fields) => {
     if (!err) {
       res.json(rows);
     } else {
@@ -23,19 +24,22 @@ router.get("/buscarProductos", (req, res) => {
   });
 });
 
+
 /////////////////////////             HECHO PERO PRESENTA FALLO          ////////////////////////////
-router.put('/editarProductos/:id', (req, res) => {
+router.put("/editarProductos/:ID_Producto", (req, res) => {
   const {Nombre,Precio,Descripcion,Disponibilidad,Cantidad,Imagen} = req.body;
-  const { ID_Producto } = req.params;
-  mysqlConnection.query(`UPDATE Productos SET Nombre=${Nombre}, Precio=${Precio}, Descripcion= ${Descripcion}, Disponibilidad=${Disponibilidad}, Cantidad${Cantidad}, Imagen=${Imagen} where ID_Producto=${ID_Producto} `,(err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'Producto actualizado'});
-    } else {
-      console.log(err);
+  const {ID_Producto} = req.params;
+  mysqlConnection.query(
+    `UPDATE Productos SET Nombre='${Nombre}',Productos.Precio='${Precio}',Productos.Descripcion= '${Descripcion}',Productos.Disponibilidad='${Disponibilidad}',Productos.Cantidad'${Cantidad}',Productos.Imagen='${Imagen}' where ID_Producto=${ID_Producto}`,
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({ status: "Producto actualizado" });
+      } else {
+        console.log(err);
+      }
     }
-  });
+  );
 });
 /////////////////////////             HECHO PERO PRESENTA FALLO          ////////////////////////////
-
 
 module.exports = router;
--- routes/registroUsuario.js
+++ routes/registroUsuario.js
@@ -3,24 +3,10 @@ const router = express.Router();
 const mysqlConnection = require("../db/db");
 
 
-
-router.get('/registrados', (req, res) => {     
-  mysqlConnection.query('SELECT * FROM Registro ', (err, rows, fields) => {
-      if (!err) {
-        res.json(rows);
-      } else {
-        console.log(err);
-      }
-    });
-  });
-
-
-
-
 router.post("/registroUsuario", (req, res) => {
   const {CorreoElectronico,Nombre,Contraseña,Vinculacion} = req.body;
   mysqlConnection.query(
-    `INSERT INTO Registro(Registro.CorreoElectronico,Registro.Nombre,Registro.Contraseña,Registro.Vinculacion) VALUES (${CorreoElectronico}' , '${Nombre}','${Contraseña}','${Vinculacion});`,
+    `INSERT INTO Registro(Registro.CorreoElectronico,Registro.Nombre,Registro.Contraseña,Registro.Vinculacion) VALUES ('${CorreoElectronico}' , '${Nombre}','${Contraseña}','${Vinculacion}');`,
     (err, rows, fields) => {
       if (!err) {
         res.json("Registrado");
@@ -29,6 +15,19 @@ router.post("/registroUsuario", (req, res) => {
       }
     }
   );
+});
+
+
+router.delete('/eliminarRegistro/:ID_Usuario', (req, res) => {
+  const {ID_Usuario} = req.params;
+  mysqlConnection.query('DELETE FROM Registro WHERE ID_Usuario = ?',
+   [ID_Usuario], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'Perfil eliminado!'});
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 
