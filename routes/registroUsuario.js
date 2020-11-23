e8932851894bb119d869bdf72d52af5868491ccb
Alejandro Rodríguez Molina <74213971+AlejoAR11@users.noreply.github.com>
2020-11-23 10:50:27 -0500

    Correciones

--- routes/registroUsuario.js
+++ routes/registroUsuario.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const mysqlConnection = require("../db/db");
 
-
 router.post("/registroUsuario", (req, res) => {
-  const {CorreoElectronico,Nombre,Contrasena} = req.body;
+  const { CorreoElectronico, Nombre, Contrasena } = req.body;
   mysqlConnection.query(
     `INSERT INTO Registro(Registro.CorreoElectronico,Registro.Nombre,Registro.Contrasena) VALUES ('${CorreoElectronico}' , '${Nombre}','${Contrasena}');`,
     (err, rows, fields) => {
@@ -17,48 +16,56 @@ router.post("/registroUsuario", (req, res) => {
   );
 });
 
-
-router.post('/login',(req,res)=>{
-  const {CorreoElectronico,Contrasena}=req.body;
-  console.log(CorreoElectronico,Contrasena)
-  mysqlConnection.query(`SELECT * FROM Registro WHERE CorreoElectronico='${CorreoElectronico}' AND contrasena='${Contrasena}'`,(err,rows,fields)=>{
-    if (err) {
-      res.json({ message:`Error`});
-      return console.log(err.message);
+router.post("/login", (req, res) => {
+  const { CorreoElectronico, Contrasena } = req.body;
+  console.log(CorreoElectronico, Contrasena);
+  mysqlConnection.query(
+    `SELECT * FROM Registro WHERE CorreoElectronico='${CorreoElectronico}' AND Contrasena='${Contrasena}'`,
+    (err, rows, fields) => {
+      if (err) {
+        res.json({ message: `Error` });
+        return console.log(err.message);
+      }
+      if (rows.length > 0) {
+        res.json({ message: `Bienvenido` });
+      } else {
+        res.json({
+          message: `Correo electronico o contraseña errada, por favor verifique la información ingresada`
+        });
+      }
     }
-    if(rows.length>0){
-      res.json({ message:`Bienvenido`});
-    }
-    else{
-      res.json({ message:`Correo electronico o contraseña errada, por favor verifique la información ingresada` });
-    }
-  });
+  );
 });
 
-router.delete('/eliminarRegistro/:ID_Usuario', (req, res) => {
-  const {ID_Usuario} = req.params;
-  mysqlConnection.query('DELETE FROM Registro WHERE ID_Usuario = ?',
-   [ID_Usuario], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'El perfil ha sido eliminado!'});
-    } else {
-      console.log(err);
-    }
-  });
-});
-
-
-router.put('/cambiarContrasena/:ID_Usuario', (req, res) => {
-  const {Contrasena} = req.body;
+router.delete("/eliminarRegistro/:ID_Usuario", (req, res) => {
   const { ID_Usuario } = req.params;
-  mysqlConnection.query(`UPDATE Registro SET Contrasena=? where ID_Usuario=?`, 
-  [Contrasena, ID_Usuario], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'Contraseña actualizada'});
-    } else {
-      console.log(err);
+  mysqlConnection.query(
+    "DELETE FROM Registro WHERE ID_Usuario = ?",
+    [ID_Usuario],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({ status: "El perfil ha sido eliminado!" });
+      } else {
+        console.log(err);
+      }
     }
-  });
+  );
+});
+
+router.put("/cambiarContrasena/:ID_Usuario", (req, res) => {
+  const { Contrasena } = req.body;
+  const { ID_Usuario } = req.params;
+  mysqlConnection.query(
+    `UPDATE Registro SET Contrasena=? where ID_Usuario=?`,
+    [Contrasena, ID_Usuario],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({ status: "Contraseña actualizada" });
+      } else {
+        console.log(err);
+      }
+    }
+  );
 });
 
 module.exports = router;
